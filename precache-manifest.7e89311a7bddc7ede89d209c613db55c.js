b5e5fccd28a36f757f9e9e12340b71a129bbc241
Yurii Rysyn <zerocool_cs@ukr.net>
2020-10-10 19:23:32 +0300

    Updates

---
+++ precache-manifest.7e89311a7bddc7ede89d209c613db55c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92021e0b380c9fe66bea45825dbfca9f",
+    "url": "/Emotional-Intellect-Presentation_EQ/index.html"
+  },
+  {
+    "revision": "277e3023cbfcdaaa9c76",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/css/main.ef7a9e98.chunk.css"
+  },
+  {
+    "revision": "7fda34e750e0e558c944",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/js/2.09ba55f7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/js/2.09ba55f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277e3023cbfcdaaa9c76",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/js/main.f4a67049.chunk.js"
+  },
+  {
+    "revision": "63ca59b7b50a6cff90c4",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/js/runtime-main.c8d534e6.js"
+  },
+  {
+    "revision": "73a46ede47dc908627274ddb8341c7b0",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/AndroidLogo.73a46ede.svg"
+  },
+  {
+    "revision": "bd480ca48788b524d6e4ef88e45da7b8",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/AppleLogo.bd480ca4.svg"
+  },
+  {
+    "revision": "e989d5efaba6287c51d4c314884051f5",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/Dots.e989d5ef.svg"
+  },
+  {
+    "revision": "f9f0e01b916e78dc113832cdab6a2c56",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/Dots2.f9f0e01b.svg"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "b484b32fcec981a533e3b9694953103b",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/SourceCodePro-Regular.b484b32f.ttf"
+  },
+  {
+    "revision": "34d049935bf7f7afe827ec796d25409d",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/blueTriangle.34d04993.svg"
+  },
+  {
+    "revision": "2fe8a70194f6ea196866c5e891397f55",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/logo.2fe8a701.svg"
+  },
+  {
+    "revision": "89dfc243a96ed7fb3e3088f434a50975",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/purpleTriangle.89dfc243.svg"
+  },
+  {
+    "revision": "ba52bc3cf74e564e45e39dd18d023561",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/slide1Img.ba52bc3c.svg"
+  },
+  {
+    "revision": "14b8ffea0686a297c2d5fec3c556ae8e",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/slide2Img.14b8ffea.svg"
+  },
+  {
+    "revision": "96fe4ffee35884a69ed635f0d4b456b7",
+    "url": "/Emotional-Intellect-Presentation_EQ/static/media/slide3Img.96fe4ffe.svg"
+  }
+]);
